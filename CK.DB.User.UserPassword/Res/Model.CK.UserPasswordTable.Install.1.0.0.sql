f6d8f750fcc22e5dd71de9630cc20b2dfbd7a23b
CENAULT <clemence_masdebail@live.fr>
2016-11-05 16:53:30 +0100

    New PasswordUser package.

---
+++ CK.DB.User.UserPassword/Res/Model.CK.UserPasswordTable.Install.1.0.0.sql
@@ -0,0 +1,14 @@
+﻿--[beginscript]
+
+create table CK.tUserPassword
+(
+	UserId int not null,
+	PwdHash varbinary(64) not null,
+	LastWriteTime datetime2(2) not null,
+	constraint PK_CK_UserPassword primary key (UserId),
+	constraint FK_CK_UserPassword_UserId foreign key (UserId) references CK.tUser(UserId)
+);
+
+insert into CK.tUserPassword( UserId, PwdHash, LastWriteTime ) values(0, 0x0, sysutcdatetime() );
+
+--[endscript]
