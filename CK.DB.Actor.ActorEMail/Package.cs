8252e9a25be55c8bff6ddbba8ad3e00836a42959
Olivier Spinelli <olivier.spinelli@invenietis.com>
2016-12-09 14:58:50 +0100

    Added missing comments.

--- CK.DB.Actor.ActorEMail/Package.cs
+++ CK.DB.Actor.ActorEMail/Package.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 
 namespace CK.DB.Actor.ActorEMail
 {
+    /// <summary>
+    /// Brings <see cref="ActorEMailTable"/> to support emails' user.
+    /// </summary>
     [SqlPackage( Schema = "CK", ResourcePath = "Res" )]
     [Versions( "1.0.0" )]
     public class Package : SqlPackage
@@ -15,6 +18,11 @@ namespace CK.DB.Actor.ActorEMail
         void Construct( Actor.Package actorPackage )
         {
         }
+
+        /// <summary>
+        /// Gets the <see cref="ActorEMailTable"/>.
+        /// </summary>
+        public ActorEMailTable ActorEMailTable { get; protected set; }
 
     }
 }
