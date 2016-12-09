293b28d1b1d07fdc309ee6f23624509529ed4f99
Olivier Spinelli <olivier.spinelli@invenietis.com>
2016-12-09 17:38:47 +0100

    Handles delayed installation of CK.DB.User.UserGoogle.AuthScope: exisitng Google users are associated to their own scpe set.

--- CK.DB.User.UserGoogle.AuthScope/Res/Model.CK.DB.User.UserGoogle.AuthScope.Package.Settle.1.0.0.sql
+++ CK.DB.User.UserGoogle.AuthScope/Res/Model.CK.DB.User.UserGoogle.AuthScope.Package.Settle.1.0.0.sql
@@ -1,7 +1,29 @@
 ﻿--[beginscript]
 
+-- Creates the default template scope set for new users.
 declare @DefaultScopeSetId int;
 exec CK.sAuthScopeSetCreate 1, N'openid', @ScopeSetIdResult = @DefaultScopeSetId output;
 update CK.tUserGoogle set ScopeSetId = @DefaultScopeSetId where UserId = 0;
+
+-- Replicates the default template on all existing UserGoogle.
+declare @UserId int;
+declare @CUser cursor;
+set @CUser = cursor local fast_forward for 
+	select UserId from CK.tUserGoogle u where u.ScopeSetId = 0;
+open @CUser;
+fetch from @CUser into @UserId;
+while @@FETCH_STATUS = 0
+begin
+	declare @NewScopeId int;
+	exec CK.sAuthScopeSetCopy @ActorId = 1, @ScopeSetId = @DefaultScopeSetId, @ForceWARStatus = 'W', @ScopeSetIdResult = @NewScopeId output
+	update CK.tUserGoogle set ScopeSetId = @NewScopeId where UserId = @UserId;
+	fetch next from @CUser into @UserId;
+end
+deallocate @CUser;
+
+-- Now that each Google user has a dedicated ScopeSet, we can ensure its unicity
+-- so that no two users can share the same ScopeSet.
+alter table CK.tUserGoogle add
+	constraint UK_CK_UserGoogle_ScopeSetId unique( ScopeSetId )
 
 --[endscript]
