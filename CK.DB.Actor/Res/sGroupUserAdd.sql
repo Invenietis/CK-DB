060f3bcab7f49bec944e9cfb5d11cbbd72fa232d
Aymeric Richard <aymeric.richard@signature-code.com>
2023-03-10 17:02:56 +0100

    Fix sGroupUserAdd throw when adding not a user:
    A simple actor is not considered as a user for example.
    This fix gives the opportunity to extend the group usage to add specific actor type.

--- CK.DB.Actor/Res/sGroupUserAdd.sql
+++ CK.DB.Actor/Res/sGroupUserAdd.sql
@@ -2,7 +2,7 @@
 --
 -- Add a User to a Group. Does nothing if the User is already in the Group.
 --
-alter procedure CK.sGroupUserAdd 
+alter procedure CK.sGroupUserAdd
 (
 	@ActorId int,
 	@GroupId int,
@@ -12,6 +12,9 @@ as begin
     if @ActorId <= 0 throw 50000, 'Security.AnonymousNotAllowed', 1;
     if @GroupId <= 0 throw 50000, 'Group.InvalidId', 1;
 
+    if not exists (select 1 from CK.tUser where UserId = @UserId)
+        throw 50000, 'User.NotAUser', 1;
+
 	-- System is, somehow, already in all groups.
     if @UserId = 1 return 0;
 
@@ -20,9 +23,9 @@ as begin
 	if @GroupId <> @UserId and not exists (select * from CK.tActorProfile where GroupId = @GroupId and ActorId = @UserId)
 	begin
 		-- If this is the System Group, only members of it can add new Users.
-		if @GroupId = 1 
+		if @GroupId = 1
 		begin
-			if not exists( select 1 from CK.tActorProfile p where p.GroupId = 1 and p.ActorId = @ActorId ) 
+			if not exists( select 1 from CK.tActorProfile p where p.GroupId = 1 and p.ActorId = @ActorId )
 			begin
 				;throw 50000, 'Security.ActorMustBeSytem', 1;
 			end
